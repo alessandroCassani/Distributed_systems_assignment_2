5e50ae17eadd1866e7ea8134cad7b90fa138c2f1
alessandroCassani <acassani@studenti.uninsubria.it>
2024-10-12 15:45:57 +0200

    client correction template

--- exercise_2/client.py
+++ exercise_2/client.py
@@ -1,37 +1,37 @@
 import socket
 from sys import argv
-from threading import Thread
-
-
-def handle_client(conn, addr):
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            print(f"Received: {data.decode()}")
-            if data == b"exit":
-                break
-            conn.sendall(data)
-        print("Closing connection to {addr}")
 
 
 def main():
+    host = None
+    port = None
     try:
-        port = int(argv[1])
+        if len(argv) > 2:
+            host = argv[1]
+            port = int(argv[2])
+        elif len(argv) > 1:
+            port = int(argv[1])
+        else:
+            raise ValueError
     except:
+        host = host or "127.0.0.1"
         port = 8080
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("0.0.0.0", port))
-        print(f"Server started on port {port}")
-        print("Waiting for a client...")
-        s.listen()
+        s.connect((host, port))
+        print("Connected to the server")
         while True:
             try:
-                conn, addr = s.accept()
-                Thread(target=handle_client, args=(conn, addr)).start()
-            except KeyboardInterrupt:
+                data = input("Enter a message: ")
+            except:
+                data = "end"
+            s.sendall(data.encode())
+            print("Message sent")
+            if data == "end":
                 break
+            response = s.recv(1024)
+            print(f"Received: {response.decode()}")
+        print("Closing connection")
 
 
 if __name__ == "__main__":
